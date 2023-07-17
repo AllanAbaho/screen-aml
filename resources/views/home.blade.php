b5141224bc4b7a2cf918a69dfe2abbcc69f11143
Allan Abaho <abahoallans@gmail.com>
2023-07-17 09:03:44 +0300

    kyc text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,8 +71,7 @@
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">Let's make KYC, Easy, Affordable and Predictable </h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">Our Anti Money Laundering Professionals are working hard, behind
-            the scenes to assure your regulator - all is well</h2>
+          <h2 data-aos="fade-up" data-aos-delay="400">The only easy, affordable and accurate anti money laundering know your customer online tool in uganda. <br>Search customers through Sanction lists, PEP lists, adverse media, National ID and company registration verification.</h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
               <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
