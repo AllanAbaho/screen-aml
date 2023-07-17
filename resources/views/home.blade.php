80332a245cbae9968b93ffe7d5073d8125074c15
Allan Abaho <abahoallans@gmail.com>
2023-07-17 12:18:18 +0300

    meta tags

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>KYC UGANDA - Home</title>
-  <meta content="" name="description">
+  <meta content="Search customers through Sanction lists, PEP lists, adverse media, National ID and company verification." name="description">
+  <meta content="The only easy, affordable and accurate anti money laundering know your customer online tool in uganda." name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
@@ -71,7 +72,7 @@
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">Let's make KYC, Easy, Affordable and Predictable </h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">The only easy, affordable and accurate anti money laundering know your customer online tool in uganda. <br>Search customers through Sanction lists, PEP lists, adverse media, National ID and company registration verification.</h2>
+          <h2 data-aos="fade-up" data-aos-delay="400">The only easy, affordable and accurate anti money laundering know your customer online tool in uganda. <br>Search customers through Sanction lists, PEP lists, adverse media, National ID and company verification.</h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
               <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
